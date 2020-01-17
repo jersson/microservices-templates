6a720158e493f1535b53b302b26acafdc3599d9d
Jersson Dongo <Jersson@gmail.com>
2020-01-17 18:44:52 -0500

    Update architecture node docker example

--- 00-node/01-examples/03-get-and-docker/core/book.js
+++ 00-node/01-examples/03-get-and-docker/core/book.js
@@ -4,7 +4,7 @@ let book = {
     getAllBooks: () => {
         let books = [
             {title: 'Code Complete 2nd Edition', author: 'Steve McConnell', year: '2004'}, 
-            {title: 'Clean Code', author: 'Robert C. Martin', year: '2008'}, 
+            {title: 'Clean Code using Docker', author: 'Robert C. Martin', year: '2008'}, 
             {title: 'The Last Lecture', author: 'Randy Pausch', year: '2008'} 
         ];
         
