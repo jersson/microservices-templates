d1f84d529a2cf8b2143611e5f4d9ff831a8e9cd8
Jersson Dongo <Jersson@gmail.com>
2020-01-17 10:32:40 -0500

    Clean architecture to node-template

--- 00-node/00-template/server.js
+++ 00-node/00-template/server.js
@@ -1,3 +1,7 @@
+//This is the post I read to build the first version of the template 
+//https://blog.cloud66.com/beginners-guide-to-building-real-world-microservices-with-node-js/
+//Thank you Mike :)
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
