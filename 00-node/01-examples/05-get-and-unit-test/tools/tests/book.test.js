10d156fb6210d2476cf82a04af05bd14ec787cf6
Jersson Dongo <Jersson@gmail.com>
2020-01-17 18:58:18 -0500

    Update architecture node unit test example

--- 00-node/01-examples/05-get-and-unit-test/tools/tests/book.test.js
+++ 00-node/01-examples/05-get-and-unit-test/tools/tests/book.test.js
@@ -1,12 +1,12 @@
 'use strict'
 const book = require('../../core/book');
 
-let books = [
+let booksForTest = [
     {title: 'Code Complete 2nd Edition', author: 'Steve McConnell', year: '2004'}, 
     {title: 'Clean Code', author: 'Robert C. Martin', year: '2008'}, 
     {title: 'The Last Lecture', author: 'Randy Pausch', year: '2008'} 
 ];
 
 test('Get all books', () => {
-    expect(book.getAllBooks()).toStrictEqual(books);
+    expect(book.getAllBooks()).toStrictEqual(booksForTest);
 });
